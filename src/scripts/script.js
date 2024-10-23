b735dd2423bbec4a8dd01cf4df0cccad651f63a9
wymuxdev <wymuxgh@gmail.com>
2024-10-22 21:07:02 -0300

    feat: implement BMI calculation

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,19 +1,61 @@
-let circularProgress = document.querySelector('.circular-progress')
-let progressValue = document.querySelector('.progress-value')
+const buttonCalc = document.querySelector('#btn-calc')
+const buttonClear = document.querySelector('#btn-clear')
+const circularProgress = document.querySelector('.circular-progress')
+const progressValue = document.querySelector('.progress-value')
 
 let progressStartValue = 0
-let progressEndValue = 21
-let speed = 60
+let progressEndValue = 0
+let resultBMI = 0
+const speed = 60
 
- let progress = setInterval(() => {
-    progressStartValue++
-
-    progressValue.textContent = `${progressStartValue}`
-    circularProgress.style.background = `conic-gradient(#9166fd ${progressStartValue * 7.2}deg, #2F2E3E ${progressStartValue * 7.2}deg)`;
-
-    if(progressStartValue === progressEndValue) {
-        clearInterval(progress)
+function infosBMI() {
+    let bmiTextInfo = document.querySelector('#bmiTextInfo')
+    let bmiMessage = ''
+    if(progressEndValue <= 18.5) {
+        bmiMessage = `${resultBMI} é abaixo do normal`
+        bmiTextInfo.textContent = bmiMessage
+    } else if (progressEndValue <=24.9) {
+        bmiMessage = `${resultBMI} está normal!`
+        bmiTextInfo.textContent = bmiMessage
+    } else if (progressEndValue <=29.9) {
+        bmiMessage = `${resultBMI} está em sobrepeso!`
+        bmiTextInfo.textContent = bmiMessage
+    } else if (progressEndValue <=34.9) {
+        bmiMessage = `${resultBMI} está em obesidade grau 1!`
+        bmiTextInfo.textContent = bmiMessage
+    } else if (progressEndValue <=39.9) {
+        bmiMessage = `${resultBMI} está em obesidade grau 2!`
+        bmiTextInfo.textContent = bmiMessage
+    } else {
+        bmiMessage = `${resultBMI} está em obesidade grau 3!`
+        bmiTextInfo.textContent = bmiMessage 
     }
-    
- }, speed);
+}
 
+function startProgress() {
+    const progress = setInterval(() => {
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}`;
+        circularProgress.style.background = `conic-gradient(#9166fd ${progressStartValue * 3.6}deg, #2F2E3E ${progressStartValue * 3.6}deg)`;
+
+        if (progressStartValue >= progressEndValue) {
+            clearInterval(progress);
+            progressValue.textContent = resultBMI
+            infosBMI()
+        }
+    }, speed);
+}
+
+ buttonCalc.addEventListener('click', function () {
+    let height = parseFloat(document.querySelector('#height').value);
+    let weight = parseFloat(document.querySelector('#weight').value);
+
+    let calcBMI = weight / (height * height);
+    const result = calcBMI.toFixed(2);
+
+    resultBMI = calcBMI.toFixed(1)
+    progressEndValue = parseFloat(result);
+
+    startProgress();
+});
