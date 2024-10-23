b28ba5d13c015413c2c68e8c5be6f792976cec97
wymuxdev <wymuxgh@gmail.com>
2024-10-22 22:05:59 -0300

    feat: add clear button and improve js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,15 +2,15 @@ const buttonCalc = document.querySelector('#btn-calc')
 const buttonClear = document.querySelector('#btn-clear')
 const circularProgress = document.querySelector('.circular-progress')
 const progressValue = document.querySelector('.progress-value')
+let bmiTextInfo = document.querySelector('#bmiTextInfo')
 
+let bmiMessage = ''
 let progressStartValue = 0
 let progressEndValue = 0
 let resultBMI = 0
-const speed = 60
+const speed = 70
 
 function infosBMI() {
-    let bmiTextInfo = document.querySelector('#bmiTextInfo')
-    let bmiMessage = ''
     if(progressEndValue <= 18.5) {
         bmiMessage = `${resultBMI} é abaixo do normal`
         bmiTextInfo.textContent = bmiMessage
@@ -34,22 +34,33 @@ function infosBMI() {
 
 function startProgress() {
     const progress = setInterval(() => {
+
         progressStartValue++;
 
         progressValue.textContent = `${progressStartValue}`;
-        circularProgress.style.background = `conic-gradient(#9166fd ${progressStartValue * 3.6}deg, #2F2E3E ${progressStartValue * 3.6}deg)`;
+        circularProgress.style.background = `conic-gradient(#9166fd ${progressStartValue * 7.2}deg, #2F2E3E ${progressStartValue * 7.2}deg)`;
 
         if (progressStartValue >= progressEndValue) {
             clearInterval(progress);
             progressValue.textContent = resultBMI
+            progressStartValue = 0
             infosBMI()
         }
     }, speed);
 }
 
  buttonCalc.addEventListener('click', function () {
+    
+    let age = parseFloat(document.querySelector('#age').value);
     let height = parseFloat(document.querySelector('#height').value);
     let weight = parseFloat(document.querySelector('#weight').value);
+
+    if(!height || !weight || !age) {
+        bmiMessage = '*Valores não preenchidos ou incorretos!*'
+        bmiTextInfo.textContent = bmiMessage
+        bmiTextInfo.classList.add('error-message')
+        return
+    }
 
     let calcBMI = weight / (height * height);
     const result = calcBMI.toFixed(2);
@@ -57,5 +68,21 @@ function startProgress() {
     resultBMI = calcBMI.toFixed(1)
     progressEndValue = parseFloat(result);
 
-    startProgress();
+    startProgress()
+
 });
+
+
+buttonClear.addEventListener('click', function() {
+    document.querySelector('#height').value = ''
+    document.querySelector('#weight').value = ''
+
+    progressValue.textContent = '0'
+
+    circularProgress.style.background = `conic-gradient(#9166fd   180deg, #2F2E3E 0deg);`;
+
+    bmiTextInfo.classList.remove('error-message')
+    bmiTextInfo.textContent = 'Sua classificação aparece aqui!'
+
+    progressStartValue = 0
+})
